86264d9a1e2cae5be4786900fb6065d0f99c47d5
lmilani <mail@lucamilani.eu>
2021-07-30 20:45:17 +0200

    CHG

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import styled from 'styled-components'
-import { useTable, usePagination } from 'react-table'
+import React from "react";
+import styled from "styled-components";
+import { useTable, usePagination } from "react-table";
 
-import makeData from './makeData'
+import makeData from "./makeData";
 
 const Styles = styled.div`
   padding: 1rem;
@@ -42,39 +42,39 @@ const Styles = styled.div`
   .pagination {
     padding: 0.5rem;
   }
-`
+`;
 
 // Create an editable cell renderer
 const EditableCell = ({
   value: initialValue,
   row: { index },
   column: { id },
-  updateMyData, // This is a custom function that we supplied to our table instance
+  updateMyData // This is a custom function that we supplied to our table instance
 }) => {
   // We need to keep and update the state of the cell normally
-  const [value, setValue] = React.useState(initialValue)
+  const [value, setValue] = React.useState(initialValue);
 
-  const onChange = e => {
-    setValue(e.target.value)
-  }
+  const onChange = (e) => {
+    setValue(e.target.value);
+  };
 
   // We'll only update the external data when the input is blurred
   const onBlur = () => {
-    updateMyData(index, id, value)
-  }
+    updateMyData(index, id, value);
+  };
 
   // If the initialValue is changed external, sync it up with our state
   React.useEffect(() => {
-    setValue(initialValue)
-  }, [initialValue])
+    setValue(initialValue);
+  }, [initialValue]);
 
-  return <input value={value} onChange={onChange} onBlur={onBlur} />
-}
+  return <input value={value} onChange={onChange} onBlur={onBlur} />;
+};
 
 // Set our editable cell renderer as the default Cell renderer
 const defaultColumn = {
-  Cell: EditableCell,
-}
+  Cell: EditableCell
+};
 
 // Be sure to pass our updateMyData and the skipPageReset option
 function Table({ columns, data, updateMyData, skipPageReset }) {
@@ -95,7 +95,7 @@ function Table({ columns, data, updateMyData, skipPageReset }) {
     nextPage,
     previousPage,
     setPageSize,
-    state: { pageIndex, pageSize },
+    state: { pageIndex, pageSize }
   } = useTable(
     {
       columns,
@@ -108,75 +108,77 @@ function Table({ columns, data, updateMyData, skipPageReset }) {
       // automatically be available on the instance.
       // That way we can call this function from our
       // cell renderer!
-      updateMyData,
+      updateMyData
     },
     usePagination
-  )
+  );
 
   // Render the UI for your table
   return (
     <>
       <table {...getTableProps()}>
         <thead>
-          {headerGroups.map(headerGroup => (
+          {headerGroups.map((headerGroup) => (
             <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map(column => (
-                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
               ))}
             </tr>
           ))}
         </thead>
         <tbody {...getTableBodyProps()}>
           {page.map((row, i) => {
-            prepareRow(row)
+            prepareRow(row);
             return (
               <tr {...row.getRowProps()}>
-                {row.cells.map(cell => {
-                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  );
                 })}
               </tr>
-            )
+            );
           })}
         </tbody>
       </table>
       <div className="pagination">
         <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
-          {'<<'}
-        </button>{' '}
+          {"<<"}
+        </button>{" "}
         <button onClick={() => previousPage()} disabled={!canPreviousPage}>
-          {'<'}
-        </button>{' '}
+          {"<"}
+        </button>{" "}
         <button onClick={() => nextPage()} disabled={!canNextPage}>
-          {'>'}
-        </button>{' '}
+          {">"}
+        </button>{" "}
         <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
-          {'>>'}
-        </button>{' '}
+          {">>"}
+        </button>{" "}
         <span>
-          Page{' '}
+          Page{" "}
           <strong>
             {pageIndex + 1} of {pageOptions.length}
-          </strong>{' '}
+          </strong>{" "}
         </span>
         <span>
-          | Go to page:{' '}
+          | Go to page:{" "}
           <input
             type="number"
             defaultValue={pageIndex + 1}
-            onChange={e => {
-              const page = e.target.value ? Number(e.target.value) - 1 : 0
-              gotoPage(page)
+            onChange={(e) => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0;
+              gotoPage(page);
             }}
-            style={{ width: '100px' }}
+            style={{ width: "100px" }}
           />
-        </span>{' '}
+        </span>{" "}
         <select
           value={pageSize}
-          onChange={e => {
-            setPageSize(Number(e.target.value))
+          onChange={(e) => {
+            setPageSize(Number(e.target.value));
           }}
         >
-          {[10, 20, 30, 40, 50].map(pageSize => (
+          {[10, 20, 30, 40, 50].map((pageSize) => (
             <option key={pageSize} value={pageSize}>
               Show {pageSize}
             </option>
@@ -184,53 +186,69 @@ function Table({ columns, data, updateMyData, skipPageReset }) {
         </select>
       </div>
     </>
-  )
+  );
 }
 
 function App() {
   const columns = React.useMemo(
     () => [
       {
-        Header: 'Name',
+        Header: "Issue",
         columns: [
           {
-            Header: 'First Name',
-            accessor: 'firstName',
+            Header: "ID",
+            accessor: "oil_iprop_id"
           },
           {
-            Header: 'Last Name',
-            accessor: 'lastName',
+            Header: "Issue Type",
+            accessor: "oil_iprop_issue_type"
           },
-        ],
+          {
+            Header: "Identifier",
+            accessor: "oil_iprop_identifier"
+          },
+          {
+            Header: "Timing",
+            accessor: "oil_iprop_timing"
+          },
+          {
+            Header: "Description",
+            accessor: "oil_iprop_description"
+          }
+        ]
       },
       {
-        Header: 'Info',
+        Header: "Info",
         columns: [
           {
-            Header: 'Age',
-            accessor: 'age',
+            Header: "Priority",
+            accessor: "oil_iprop_priority"
           },
           {
-            Header: 'Visits',
-            accessor: 'visits',
+            Header: "Owner",
+            accessor: "oil_iprop_owner"
           },
           {
-            Header: 'Status',
-            accessor: 'status',
+            Header: "Target Resolution Date",
+            accessor: "oil_iprop_target_resolution_date"
           },
           {
-            Header: 'Profile Progress',
-            accessor: 'progress',
+            Header: "Status",
+            accessor: "oil_iprop_status"
           },
-        ],
-      },
+          {
+            Header: "Action",
+            accessor: "oil_iprop_action"
+          }
+        ]
+      }
     ],
     []
-  )
+  );
 
-  const [data, setData] = React.useState(() => makeData(20))
-  const [originalData] = React.useState(data)
-  const [skipPageReset, setSkipPageReset] = React.useState(false)
+  const [data, setData] = React.useState(() => makeData(1));
+  const [originalData] = React.useState(data);
+  const [skipPageReset, setSkipPageReset] = React.useState(false);
 
   // We need to keep the table from resetting the pageIndex when we
   // Update data. So we can keep track of that flag with a ref.
@@ -240,30 +258,30 @@ function App() {
   // original data
   const updateMyData = (rowIndex, columnId, value) => {
     // We also turn on the flag to not reset the page
-    setSkipPageReset(true)
-    setData(old =>
+    setSkipPageReset(true);
+    setData((old) =>
       old.map((row, index) => {
         if (index === rowIndex) {
           return {
             ...old[rowIndex],
-            [columnId]: value,
-          }
+            [columnId]: value
+          };
         }
-        return row
+        return row;
       })
-    )
-  }
+    );
+  };
 
   // After data chagnes, we turn the flag back off
   // so that if data actually changes when we're not
   // editing it, the page is reset
   React.useEffect(() => {
-    setSkipPageReset(false)
-  }, [data])
+    setSkipPageReset(false);
+  }, [data]);
 
   // Let's add a data resetter/randomizer to help
   // illustrate that flow...
-  const resetData = () => setData(originalData)
+  const resetData = () => setData(originalData);
 
   return (
     <Styles>
@@ -275,7 +293,7 @@ function App() {
         skipPageReset={skipPageReset}
       />
     </Styles>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/makeData.js
+++ src/makeData.js
@@ -1,40 +1,43 @@
-import namor from 'namor'
+import namor from "namor";
 
-const range = len => {
-  const arr = []
+const range = (len) => {
+  const arr = [];
   for (let i = 0; i < len; i++) {
-    arr.push(i)
+    arr.push(i);
   }
-  return arr
-}
+  return arr;
+};
 
-const newPerson = () => {
-  const statusChance = Math.random()
+const newSampleIssue = () => {
+  const statusChance = Math.random();
   return {
-    firstName: namor.generate({ words: 1, numbers: 0 }),
-    lastName: namor.generate({ words: 1, numbers: 0 }),
-    age: Math.floor(Math.random() * 30),
-    visits: Math.floor(Math.random() * 100),
-    progress: Math.floor(Math.random() * 100),
-    status:
+    oil_iprop_id: 1,
+    oil_iprop_issue_type: namor.generate({ words: 1, numbers: 0 }),
+    oil_iprop_timing: Math.floor(Math.random() * 30),
+    oil_iprop_description: Math.floor(Math.random() * 100),
+    oil_iprop_priority: Math.floor(Math.random() * 100),
+    oil_iprop_owner: namor.generate({ words: 1, numbers: 0 }),
+    oil_iprop_target_resolution_date: namor.generate({ date: 1 }),
+    oil_iprop_status:
       statusChance > 0.66
-        ? 'relationship'
+        ? "relationship"
         : statusChance > 0.33
-        ? 'complicated'
-        : 'single',
-  }
-}
+        ? "complicated"
+        : "single",
+    oil_iprop_action: namor.generate({ workds: 1 })
+  };
+};
 
 export default function makeData(...lens) {
   const makeDataLevel = (depth = 0) => {
-    const len = lens[depth]
-    return range(len).map(d => {
+    const len = lens[depth];
+    return range(len).map((d) => {
       return {
-        ...newPerson(),
-        subRows: lens[depth + 1] ? makeDataLevel(depth + 1) : undefined,
-      }
-    })
-  }
+        ...newSampleIssue(),
+        subRows: lens[depth + 1] ? makeDataLevel(depth + 1) : undefined
+      };
+    });
+  };
 
-  return makeDataLevel()
+  return makeDataLevel();
 }
